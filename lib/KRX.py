657ee0281817a0d4b8b1c3e96701c9a4488df88a
jjeamin <woalsdl500@naver.com>
2020-05-08 00:25:43 +0900

    Update KRX function price, code, company, foreign info

--- lib/KRX.py
+++ lib/KRX.py
@@ -1,12 +1,9 @@
-import json
-import time
 import requests
 import pandas as pd
-from pandas import json_normalize
 from io import BytesIO
 
 
-def get_per(fromdata='20080101', todate='20200505', period='day', type='kospi'):
+def get_per(fromdate='20080101', todate='20200505', period='day', type='kospi'):
     get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
 
     query_str_params = {
@@ -15,7 +12,7 @@ def get_per(fromdata='20080101', todate='20200505', period='day', type='kospi'):
         'url': "MKD/13/1301/13010104/mkd13010104_02",
         'type': type,
         'period_selector': period,
-        'fromdate': fromdata,
+        'fromdate': fromdate,
         'todate': todate,
         'pagePath': "/contents/MKD/13/1301/13010104/MKD13010104.jsp"
     }
@@ -43,7 +40,11 @@ def get_per(fromdata='20080101', todate='20200505', period='day', type='kospi'):
     return df
 
 
-def get_kospi(date='20010228'):
+def get_kospi200(date='20010228'):
+    """
+    지수
+    주가지수 -> KOSPI시리즈
+    """
     get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
 
     query_str_params = {
@@ -81,7 +82,77 @@ def get_kospi(date='20010228'):
     return df
 
 
-def get_company(date='20010228'):
+def get_ticker(fromdate='20080101', todate='20200505', code='KR7005930003'):
+    """
+    통계
+    주식 -> 종목시세 -> 전체종목 시세
+    """
+    get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+
+    query_str_params = {
+        'name': 'fileDown',
+        'filetype': 'xls',
+        'url': 'MKD/04/0402/04020100/mkd04020100t3_02',
+        'isu_cd': code,
+        'fromdate': fromdate,
+        'todate': todate,
+        'pagePath': '/contents/MKD/04/0402/04020100/MKD04020100T3T2.jsp'
+    }
+
+    header_data = {
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    r = requests.get(get_req_url, query_str_params, headers=header_data)
+
+    gen_req_url = 'http://file.krx.co.kr/download.jspx'
+    headers = {
+        'Referer': "http://marketdata.krx.co.kr/mdi",
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    form_data = {
+        'code': r.content
+    }
+
+    r = requests.post(gen_req_url, form_data, headers=headers)
+
+    df = pd.read_excel(BytesIO(r.content))
+
+    return df
+
+
+def get_comany():
+    url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx"
+    market = 'ALL'
+
+    params = {
+        'name': 'form',
+        'bld': 'COM/finder_stkisu'
+    }
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    resp = requests.get(url, headers=headers, params=params)
+
+    params = ({'mktsel': market,
+               'searchText': ""})
+
+    params.update({'code': resp.text})
+
+    url = "http://marketdata.krx.co.kr/contents/MKD/99/MKD99000001.jspx"
+
+    resp = requests.post(url, headers=headers, data=params)
+
+    df = pd.DataFrame(resp.json()['block1'])
+
+    return df
+
+
+def get_total_price(date='20010228'):
+    """
+    통계
+    주식 -> 종목시세 -> 전체종목 시세
+    """
     get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
 
     query_str_params = {
@@ -117,44 +188,96 @@ def get_company(date='20010228'):
     return df
 
 
-# def get_company():
-#     """
-#     Reference : https://github.com/FinanceData/FinanceDataReader/
-#     """
-#
-#     # company
-#     url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download'
-#     df_company = pd.read_html(url, header=0)[0]
-#     df_company = df_company[['회사명', '종목코드']]
-#
-#     df_company = df_company.rename(columns={'회사명': 'name',
-#                                             '종목코드': 'code'})
-#
-#     df_company['code'] = df_company['code'].apply(lambda x: '{:06d}'.format(x))
-#
-#     # market name
-#     header_data = {'User-Agent': 'Chrome/78.0.3904.87 Safari/537.36', }
-#
-#     url = f'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx?bld=COM%2Ffinder_stkisu&name=form&_={int(time.time() * 1000)}'
-#     r = requests.get(url, headers=header_data)
-#
-#     down_url = 'http://marketdata.krx.co.kr/contents/MKD/99/MKD99000001.jspx'
-#     down_data = {
-#         'mktsel': 'ALL',
-#         'pagePath': '/contents/COM/FinderStkIsu.jsp',
-#         'code': r.content,
-#         'geFirstCall': 'Y',
-#     }
-#
-#     r = requests.post(down_url, down_data, headers=header_data)
-#     data = json.loads(r.text)
-#
-#     df_finder = json_normalize(data, 'block1')
-#     df_finder.columns = ['fullcode', 'shortcode', 'name', 'market']
-#     df_finder['code'] = df_finder['shortcode'].str[1:]
-#     df_finder = df_finder[['market', 'code']]
-#
-#     # merge
-#     df = pd.merge(df_company, df_finder, how='left', left_on='code', right_on='code')
-#
-#     return df
+def get_reference(date):
+    # No 공휴일
+    get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+
+    if isinstance(date, str):
+        query_str_params = {
+            'name': 'fileDown',
+            'filetype': 'xls',
+            'url': 'MKD/13/1302/13020401/mkd13020401',
+            'market_gubun': 'STK',
+            'gubun': '1',
+            'schdate': date,
+            'pagePath': '/contents/MKD/13/1302/13020401/MKD13020401.jsp',
+        }
+    elif isinstance(date, list):
+        query_str_params = {
+            'name': 'fileDown',
+            'filetype': 'xls',
+            'url': 'MKD/13/1302/13020401/mkd13020401',
+            'market_gubun': 'STK',
+            'gubun': '2',
+            'isu_cdnm': 'A005930/삼성전자',
+            'isu_cd': 'KR7005930003',
+            'isu_srt_cd': 'A005930',
+            'fromdate': date[0],
+            'todate': date[1],
+            'pagePath': '/contents/MKD/13/1302/13020401/MKD13020401.jsp',
+        }
+    else:
+        ValueError("data is str, list")
+
+    header_data = {
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    r = requests.get(get_req_url, query_str_params, headers=header_data)
+
+    gen_req_url = 'http://file.krx.co.kr/download.jspx'
+    headers = {
+        'Referer': "http://marketdata.krx.co.kr/contents/MKD/13/1301/13010104/MKD13010104.jsp",
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    form_data = {
+        'code': r.content
+    }
+
+    r = requests.post(gen_req_url, form_data, headers=headers)
+
+    df = pd.read_excel(BytesIO(r.content))
+
+    return df
+
+
+def get_foreign_reserves(date='20010228'):
+    """
+    통계
+    주식 -> 종목시세 -> 전체종목 시세
+    """
+    get_req_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+
+    query_str_params = {
+        'name': 'fileDown',
+        'filetype': 'xls',
+        'url': 'MKD/13/1302/13020402/mkd13020402',
+        'market_gubun': 'STK',
+        'lmt_tp': '1',
+        'sect_tp_cd': 'ALL',
+        'schdate': date,
+        'pagePath': '/contents/MKD/13/1302/13020402/MKD13020402.jsp'
+    }
+
+    header_data = {
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    r = requests.get(get_req_url, query_str_params, headers=header_data)
+
+    gen_req_url = 'http://file.krx.co.kr/download.jspx'
+    headers = {
+        'Referer': "http://marketdata.krx.co.kr/mdi",
+        'User-Agent': 'Chrome/78 Safari/537',
+    }
+
+    form_data = {
+        'code': r.content
+    }
+
+    r = requests.post(gen_req_url, form_data, headers=headers)
+
+    df = pd.read_excel(BytesIO(r.content))
+
+    return df
