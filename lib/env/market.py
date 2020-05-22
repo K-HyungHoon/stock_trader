7133634bdc5312285d4b0c2cae5e44c87e5a4c6f
jjeamin <woalsdl500@naver.com>
2020-05-23 04:38:16 +0900

    Update Env, Agent

--- lib/env/market.py
+++ lib/env/market.py
@@ -1,12 +1,15 @@
 import os
+import numpy as np
 import gym
 from gym import spaces
 
 
 class Kospi200_Env(gym.Env):
-    def __init__(self, history):
+    def __init__(self, history, labels, window_size=5):
         super(Kospi200_Env, self).__init__()
         self.history = history
+        self.labels = labels
+        self.window_size = window_size
 
         period, num_company, num_feature = self.history.shape
 
@@ -16,13 +19,21 @@ class Kospi200_Env(gym.Env):
         self.observation_space = spaces.Box(0, 100, shape=(num_feature, period))
 
     def step(self, action):
-        NotImplementedError()
+        self.current_step += 1
 
     def reset(self):
-        NotImplementedError()
+        self.current_step = 0
+
+        return self._next_observation()
+
+    def _next_observation(self):
+        obs = np.array([])
+
+        return obs
 
     def render(self, mode='human'):
-        NotImplementedError()
+        if mode == 'print':
+            print(f'STEP : {self.current_step}')
 
     def close(self):
         NotImplementedError()
--- main.py
+++ main.py
@@ -1,15 +1,47 @@
+import os
+import tensorflow as tf
 import numpy as np
+import argparse
 from lib.env import Kospi200_Env
+from lib.data import get_data
+from lib.agent.PG import PG
+# parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_episode', type=int, default=1)
+parser.add_argument('--window_size', type=int, default=5)
+parser.add_argument('--path', type=str, default="data/KOSPI200")
+args = parser.parse_args()
 
-data_path = './kospi200.npy'
+# get data
+datas, labels = get_data(args.path)
 
-samples = np.load(data_path)
+# parameter
+input_shape = (datas.shape[1], args.window_size, datas.shape[2])
+optimizer = tf.keras.optimizers.Adam()
+loss = 'binary_crossentropy'
 
-env = Kospi200_Env(samples)
+# agent
+agent = PG(input_shape, optimizer, loss)
+agent.summary()
 
-print(env.action_space)
-print(env.observation_space)
+# env
+env = Kospi200_Env(datas, labels, window_size=args.window_size)
 
-# for _ in range(1000):
-    # state = env.step(action)
-    # action = Network(state)
+# train
+for e in range(args.num_episode):
+    # state = np.random.random((1, 200, 10, 5))
+    state = env.reset()
+
+    while True:
+        env.render(mode='print')
+
+        action = agent.get_action(state)
+        next_state, reward = env.step(action)
+
+        loss = agent.learn(state, reward, action)
+
+        print(loss)
+
+        state = next_state
+
+        break
